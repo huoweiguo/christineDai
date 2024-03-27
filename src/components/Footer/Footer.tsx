e0d76f790f2564b24d6928412b6ccadffa010cc9
srd18056414952 <liuhuan@youzi-tech.cn>
2024-03-27 20:44:13 +0800

    feat：完成尾部样式

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -15,6 +15,17 @@ export default function Footer() {
             <li>联系我们</li>
           </ul>
         </div>
+        <div className={styles['footer-soc']}>
+          <img src={require('../../assets/images/wb.png')} alt='微博'/>
+          <img src={require('../../assets/images/wx.png')} alt='微信'/>
+          <img src={require('../../assets/images/xhs.png')} alt='小红书'/>
+          <img src={require('../../assets/images/instagram.png')} alt=''/>
+        </div>
+      </div>
+      <div className={styles['footer-copy']}>
+        <span>沪ICP备2022010059号-1</span>
+        <span className={styles['footer-line']}>|</span>
+        Copyright © 2023 Chirstine Dai
       </div>
     </div>
   )
