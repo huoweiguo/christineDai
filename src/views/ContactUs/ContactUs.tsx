5a2070244bb29959c9158b212a0e0eabba0c6d55
chengrui333 <1941996077@qq.com>
2024-04-01 15:27:45 +0800

    change

--- src/views/ContactUs/ContactUs.tsx
+++ src/views/ContactUs/ContactUs.tsx
@@ -6,6 +6,7 @@ import Footer from '../../components/Footer/Footer'
 export default function ContactUs() {
 
   const [ activeIndex, setactiveIndex ] = useState<number>(0)
+  const [ selectIndex, setSelectIndex ] = useState<number>(0)
   
   const bgData: any[] = [
     {
@@ -23,49 +24,54 @@ export default function ContactUs() {
     setactiveIndex(index)
   }
 
+  const openContactForm = (index: number) => {
+    setSelectIndex(index)
+  }
+
   return (
     <div className={ styles.contactUs }>
       <Header></Header>
       <div className={ styles['cu-container'] } style={bgData[activeIndex]}>
-        <div className={styles.itemBox} onMouseEnter={()=>handleMouseEnter(0)}>
-          <div className={styles.wrapper}>
+        <div className={`${styles.itemBox} ${selectIndex && selectIndex != 1 ? styles.narrow : ''}`} onMouseEnter={()=>handleMouseEnter(0)}>
+          <div className={`${styles.wrapper} ${selectIndex == 1 ? styles.showWrapper : ''}`}>
             <div className={styles.title}>
               巴黎<br/>Paris
             </div>
-            <div className={styles.content}>
+            <div className={`${styles.content } ${selectIndex == 1 ? styles.showContent : ''}`}>
               <p>Art Jewelry Appreciation Room</p>
               <p>Reservation Appreciation</p>
               <p className={styles['mg-25']}>Email:official@christine-dai.com</p>
-              <p className={styles['c-btn']}>Contact Us</p>
+              <p className={styles['c-btn']} onClick={()=>openContactForm(1)}>Contact Us</p>
             </div>
           </div>
         </div>
-        <div className={styles.itemBox} onMouseEnter={()=>handleMouseEnter(1)}>
-          <div className={styles.wrapper}>
+        <div className={`${styles.itemBox} ${selectIndex && selectIndex != 2 ? styles.narrow : ''}`} onMouseEnter={()=>handleMouseEnter(1)}>
+          <div className={`${styles.wrapper} ${selectIndex == 2 ? styles.showWrapper : ''}`}>
             <div className={styles.title}>
               巴黎<br/>Paris
             </div>
-            <div className={styles.content}>
+            <div className={`${styles.content } ${selectIndex == 2 ? styles.showContent : ''}`}>
               <p>Art Jewelry Appreciation Room</p>
               <p>Reservation Appreciation</p>
               <p className={styles['mg-25']}>Email:official@christine-dai.com</p>
-              <p className={styles['c-btn']}>Contact Us</p>
+              <p className={styles['c-btn']} onClick={()=>openContactForm(2)}>Contact Us</p>
             </div>
           </div>
         </div>
-        <div className={styles.itemBox} onMouseEnter={()=>handleMouseEnter(2)}>
-          <div className={styles.wrapper}>
+        <div className={`${styles.itemBox} ${selectIndex && selectIndex != 3 ? styles.narrow : ''}`} onMouseEnter={()=>handleMouseEnter(2)}>
+          <div className={`${styles.wrapper} ${selectIndex == 3 ? styles.showWrapper : ''}`}>
             <div className={styles.title}>
               巴黎<br/>Paris
             </div>
-            <div className={styles.content}>
+            <div className={`${styles.content } ${selectIndex == 3 ? styles.showContent : ''}`}>
               <p>Art Jewelry Appreciation Room</p>
               <p>Reservation Appreciation</p>
               <p className={styles['mg-25']}>Email:official@christine-dai.com</p>
-              <p className={styles['c-btn']}>Contact Us</p>
+              <p className={styles['c-btn']} onClick={()=>openContactForm(3)}>Contact Us</p>
             </div>
           </div>
         </div>
+        <div className={`${styles['contact-form']} ${selectIndex ? styles.enlarge : ''}`}></div>
       </div>
       <Footer></Footer>
     </div>  
