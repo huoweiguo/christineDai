3c0b6adfbb782c8968fa11a9fb42651fb74d39be
huoweiguo <huoweiguo123>
2024-04-03 09:55:48 +0800

    router mode hash

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,4 +1,4 @@
-import { createHashRouter } from 'react-router-dom'
+import { createHashRouter, createBrowserRouter } from 'react-router-dom'
 import type { RouteObject } from 'react-router-dom'
 import React, { lazy } from 'react'
 
