7c167cd1b657f092118f28a49d152739952f598a
srd18056414952 <liuhuan@youzi-tech.cn>
2024-05-03 17:44:45 +0800

    feat:更换标题

--- src/views/Layout/Layout.tsx
+++ src/views/Layout/Layout.tsx
@@ -2,10 +2,12 @@ import React,{useState, useEffect} from 'react'
 import { Outlet } from 'react-router-dom'
 import styles from './Layout.module.scss'
 import Nav from '../../components/Nav/Nav'
-import { useLocation } from 'react-router-dom';
+import { useLocation,matchRoutes } from 'react-router-dom';
+import { routes } from '../../router'
 export default function Layout() {
   const location = useLocation();
   const [open, setOpen] = useState<boolean>(location.pathname==='/');
+  const [name, setName] = useState<string>('');
   const openChange = (newValue:boolean) => {
     setTimeout(() => {
       setOpen(newValue);
@@ -15,7 +17,12 @@ export default function Layout() {
     if(location.pathname === '/'){
       setOpen(true);
     }  
-  }, [location.pathname])
+    const matchs = matchRoutes(routes, location)
+    if (Array.isArray(matchs)) {
+      const title = matchs[matchs.length - 1].route.meta?.title as string
+      setName(title)
+    } 
+  }, [location, location.pathname])
   return (
     <div className={styles.LayoutBody}>
       <div className={styles.Outlet}>
@@ -32,7 +39,7 @@ export default function Layout() {
           C
         </div>
         <div className={styles['burger-text']}>
-          艺术珠宝
+          {name}
         </div>
       </div>
       <div className={open?`${styles.rightBody}`:`${styles.rightBody} ${styles.rightBody2}`}>
