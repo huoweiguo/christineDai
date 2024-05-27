c2cf1bd0d404851bcd939e6602bd000bb3666a52
srd18056414952 <liuhuan@youzi-tech.cn>
2024-05-27 18:22:17 +0800

    feat:修改视频时间轴

--- src/views/GlobalMedia/GlobalMedia.tsx
+++ src/views/GlobalMedia/GlobalMedia.tsx
@@ -10,22 +10,86 @@ interface typeData{
   title: string,
   media_img: string,
   linkurl: string,
-  created_at: string
+  created_at: string,
+  top?: number
 }
 
 export default function GlobalMedia() {
   const navigate = useNavigate();
-  const [isFix,setIsFix] = useState<boolean>(true)
+  const [isFix,setIsFix] = useState<boolean>(false)
   const [navNumber,setNavNumber] = useState<Number>(0)
+  let globalListBak: typeData[] = []
   const handleButtonClick = (url:string) => {
     // 使用 navigate() 方法进行路由跳转
     window.location.href = url
   };
   const [mediaData, setMediaData] = useState<typeData[]>([])
+  const [mediaData2, setMediaData2] = useState<typeData[]>([])
+  const cutNav = (i:number, top: number) =>{
+    window.scrollTo({
+      top,
+      behavior: 'smooth'
+    })
+    
+    setTimeout(() => {
+      setNavNumber(i)
+    }, 700)
+  }
+  const getElementPageY = () => {
+    const docUI: HTMLUListElement  = document.querySelector('#docUI') as HTMLUListElement
+    const children  = docUI.querySelectorAll('li')
+    // 使用Set来存储唯一的offsetTop值
+    const seenOffsets = new Set<number>();
+    const uniqueChildren: HTMLLIElement[] = [];
+
+    // 遍历children并去重
+    children.forEach(child => {
+      const offsetTop = child.offsetTop;
+      if (!seenOffsets.has(offsetTop)) {
+        seenOffsets.add(offsetTop);
+        uniqueChildren.push(child);
+      }
+    });
+    console.log(uniqueChildren,'uniqueChildren')
+    // 更新globalListBak
+    uniqueChildren.forEach((child, i) => {
+      console.log(globalListBak[i].top,'globalListBak[i].top')
+      globalListBak[i].top = child.offsetTop + 220;
+    });
+
+    setMediaData2(globalListBak)
+  }
+  const listenerULList = () => {
+    const scrollY = window.scrollY
+    const len = globalListBak.length
+    if(scrollY>300){
+      setIsFix(true)
+    }else{
+      setIsFix(false)
+    }
+    for (let i = 0; i < len; i++) {
+      if (((globalListBak[i].top) as number + 220) >= scrollY) {
+        setNavNumber(i)
+        return false
+      }
+    }
+  }
   useEffect(()=>{
     getMediaList().then(res=>{
       if(res.data.code===200){
-        setMediaData([...res.data.data.list])
+        const list = res.data.data.list
+        const list2 = [...list,...list,...list,...list,...list]
+        setMediaData(list2)
+        for (let i = 0; i < list2.length; i += 3) {
+          globalListBak.push({
+            ...list2[i],
+            top:0
+          });
+        }
+        setTimeout(() => {
+          getElementPageY()
+          window.addEventListener('scroll', listenerULList, false)
+        },500)
       }
     })
   },[])
@@ -41,26 +105,26 @@ export default function GlobalMedia() {
       </div>
       <div className={styles['gw-container']}>
       <div className={isFix?styles['global-nav-fix']:styles['global-nav']}>
-          <div className={styles['nav-line']}></div>
+        <div className={styles['nav-line']}></div>
           <ul className={styles['nav-list']}>
-            {mediaData.map((item,index)=>{
-              return(<li className={navNumber === index?styles.active:styles.navItem} key={item.id}>
+            {mediaData2.map((item,index)=>{
+              return(<li onClick={()=>cutNav(index, item.top as number)} className={navNumber === index?styles.active:styles.navItem} key={item.id}>
                 <div className={styles.activeText} dangerouslySetInnerHTML={{ __html: item.title }}></div>
               </li>)
             })}
           </ul>
         </div>
         <div className={styles['w-1400']}>
-          <div className={styles['content-box']}>
+          <ul className={styles['content-box']} id="docUI">
             { mediaData.length > 0 && mediaData.map(item=>(
-              <div onClick={()=>handleButtonClick(item.linkurl)} className={styles.card} key={item.id}>
+              <li onClick={()=>handleButtonClick(item.linkurl)} className={styles.card} key={item.id}>
                 <div className={styles.imgBox}>
                   <img src={item.media_img} alt=""/>
                 </div>
                 <h5 dangerouslySetInnerHTML={{ __html: item.title }}></h5>
-              </div>
+              </li>
             ))}
-          </div>
+          </ul>
         </div>
       </div>
       <Footer></Footer>
